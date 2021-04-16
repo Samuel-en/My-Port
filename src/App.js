ccb36e9656ef4d4a78a3cc06eea8df0d37a111c2
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-04-15 20:21:19 -0400

    installed react-router-dom, imported BrowserRouter, added NavBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss' ;
 
+
 function App() {
   return (
     <div className="App">
-      <div className="sidebar"></div>
-      <div className="maincontent"></div>
+      <div className="sidebar">
+        <NavBar />
       </div>
+      <div className="main-content">
+      </div>
+    </div>
   );
 }
 
