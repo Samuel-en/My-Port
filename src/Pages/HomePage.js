4edd242b2d3c11e58ea83cc438e10086b3c6832c
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-05-05 00:29:49 -0400

    created typography.scss file, to style homepage text, imported the file into app.scss

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
-    return (
-        <div className="HomePage">
-            <header className="hero">
-                <h1 className="hero-text">
-                Hi, My name is Samuel 
-                <span>I am Fullstack Web Developer</span>
-                </h1>
-            </header>
-        </div>
-    )
+  return (
+    <div className="HomePage">
+      <header className="hero">
+        <h1 className="hero-text">
+          Hello! I am Samuel 
+          <span>Full Stack Developer </span>
+        </h1>
+        <p className="h-sub-text">
+          I design and develop software, browse through my portfolios
+          for some awesome projects!
+
+        </p>
+        <div className="Icons"/>
+      </header>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
