58d237c350b2f5769215b8bd5bda0dccf11daf6c
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-05-06 00:00:53 -0400

    added icons through font awesome and styled accordingly.

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,19 +1,25 @@
 import React from "react";
-
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+// import {falinkdin} from '@fortawesome/free-brands-svg-icons';
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function HomePage() {
   return (
     <div className="HomePage">
       <header className="hero">
         <h1 className="hero-text">
-          Hello! I am Samuel 
-          <span>Full Stack Developer </span>
+          Samuel 
+          <span> Full Stack Developer </span>
         </h1>
         <p className="h-sub-text">
-          I design and develop software, browse through my portfolios
+          designs and develops software, browse through portfolio
           for some awesome projects!
-
         </p>
-        <div className="Icons"/>
+        <div className="Icons">
+        <Link className ="icon-holder">
+            <FontAwesomeIcon icon={faGithub} className="gh"/>
+        </Link>
+      </div>
       </header>
     </div>
   );
