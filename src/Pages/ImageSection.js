bd73b3fd2cb47db4f1a7020b2913715cbacc06ea
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-05-08 14:57:07 -0400

    added a button on the about me section for my resume, styled button. Added additional information on my about me section.

--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -8,19 +8,39 @@ function ImageSection() {
         <img src={about} alt=""/>
       </div>
       <div className="about-info">
-        <h4>I am<span> Samuel Endrias</span></h4> 
-        <p>
+        <h4>I am<span> Full Stack Software Developer</span></h4> 
+        <p className="about-text">
           Nulla consequat sint Lorem tempor deserunt ad officia. Nulla do irure
           ex eu commodo ut laborum exercitation amet minim in nulla ea
           voluptate. Qui nulla voluptate adipisicing nostrud culpa culpa
           cupidatat nostrud eiusmod aute.
         </p>
           <div className="about-details">
-              
-      </div>
+          <div className="left-section">
+                 <p>Full Name</p>
+                 <p>Pronoun</p>
+                 <p>Nationality</p>
+                 <p>Languages</p>
+                 <p>Currently</p>
+                 <p>Lived/visited</p>
+
+            </div>
+            <div className="right-section">
+                 <p>: Samuel Endrias</p>
+                 <p>: He/Him</p>
+                 <p>: Eritrean-British-American</p>
+                 <p>: English, Tigrigna </p>                 
+                 <p>: Atlanta, Georgia USA </p>
+                 <p>: Eritrea, Ethiopia, United Kingdom,
+                      Germany, Netharlands, France</p>
+            </div>
+        </div>
+        <button className="btn">Resume</button>
     </div>
-    </div>
-  );
+</div>
+
+
+);
 }
 
 export default ImageSection;
