0742896d37991b7ffeedaf11c4cda5d5231ee0bd
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-05-08 17:10:05 -0400

    added progress bars to skills section

---
+++ src/Components/Skills.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Skills({skill, progress, width}) {
+    return (
+        <div>
+            <div className="Skills">
+            <div className="skill-container">
+                <h5 className="skill-title">{skill}</h5>
+                <div className="skill-bar">
+                    <p className="skill-text">{progress}</p>
+                    <div className="skill-progress">
+                        <div className="progress">
+                            <div className="inner-pregress"style={{width:width}}>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Tittle from '../Components/Tittle';
-import ImageSection from './ImageSection';
-
+import ImageSection from '../Components/ImageSection';
+import Skills from '../Components/Skills';
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Tittle title= {'About Me'} span={'About Me'} />
             <ImageSection />
+            <Tittle title= {'My Skills'} span={'My Skills'} />
+            <div className="skills-container">
+            <Skills skill={'Javascript'} progress={'70%'} width={'70%'} />
+            <Skills skill={'React.Js'} progress={'70%'} width={'70%'} />
+            <Skills skill={'Node.Js'} progress={'80%'} width={'80%'} />
+            <Skills skill={'Express.Js'} progress={'60%'} width={'60%'} />
+            <Skills skill={'SQL'} progress={'70%'} width={'70%'} />
+            <Skills skill={'MySQL'} progress={'50%'} width={'50%'} />
+            <Skills skill={'HTML'} progress={'80%'} width={'80%'} />
+            <Skills skill={'CSS'} progress={'80%'} width={'80%'} />
+            </div>
         </div>
     )
 }
